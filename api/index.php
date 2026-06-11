da87b42be10baa5ca58d04b3779a2abd33ee2c10
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2026-06-11 23:27:21 +0700

    chore: temporary file list debug in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
-<?php
-// Forward Vercel requests to Laravel's index.php
-require __DIR__ . '/../public/index.php';
+echo "<pre>";
+echo "Current directory: " . __DIR__ . "\n";
+echo "Root directory: " . realpath(__DIR__ . '/..') . "\n";
+echo "Files in root:\n";
+function listFolderFiles($dir, $depth = 0) {
+    if ($depth > 3) return; // avoid too deep scan
+    $ffs = @scandir($dir);
+    if (!$ffs) return;
+    unset($ffs[array_search('.', $ffs, true)], $ffs[array_search('..', $ffs, true)]);
+    foreach($ffs as $ff){
+        $path = $dir.'/'.$ff;
+        echo $path."\n";
+        if(is_dir($path) && $ff !== 'vendor' && $ff !== 'node_modules') {
+            listFolderFiles($path, $depth + 1);
+        }
+    }
+}
+listFolderFiles(realpath(__DIR__ . '/..'));
+echo "</pre>";
+exit;
