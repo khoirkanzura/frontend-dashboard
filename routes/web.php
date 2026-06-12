97c975a0818939818368232b8af1bdf095ae952d
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2026-06-12 14:54:15 +0700

    fix: correct route definition for the home page to return the welcome view

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\SettingsController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/loans', [LoanController::class, 'index'])->name('loans');
 Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
 Route::post('/settings/update', [SettingsController::class, 'update'])->name('settings.update');
-
