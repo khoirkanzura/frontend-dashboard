72e4be2f5b5df8322fc927b9e425aa1f37e66870
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2026-06-12 20:04:36 +0700

    fix vite build

--- vite.config.js
+++ vite.config.js
@@ -12,11 +12,5 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
-    ],
-
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true
-    }
+    ]
 });
