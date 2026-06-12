278c2089ca4804caf5f172dff686de44d9ed43a3
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2026-06-12 16:50:48 +0700

    fix vite manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        manifest: 'manifest.json',
+    },
+
     plugins: [
         laravel({
             input: [
